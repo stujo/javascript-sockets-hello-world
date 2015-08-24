eccfcbbb468a60ed33a48a107a75eebd7e0972a1
Stujo <stujo@github.com>
2015-08-24 11:43:49 -0700

    Add jQuery and callback to use socket emit when user enters a message

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+
 });
 
 
